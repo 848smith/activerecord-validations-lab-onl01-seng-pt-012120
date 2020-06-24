57e479ad31e487f4c95200f85d7ef46523046e1b
Andrew Smith <848smith@gmail.com>
2020-06-24 03:50:26 +0000

    Automatically backed up by Learn

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -2,7 +2,7 @@ class PostValidator < ActiveModel::Validator
   def validate(record)
     if record.title
       array = ["Won't Believe", "Secret", "Top [number]", "Guess"]
-      if array.any? {|string| record.title.include?(string) }
+      if array.detect {|string| record.title.include?(string) }.nil?
         record.errors[:title] << "Has to contain clickbait"
       end
     end
