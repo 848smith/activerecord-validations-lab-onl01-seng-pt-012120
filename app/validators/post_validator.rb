10e8ade4b062ee4a1f2a8e8fec6ff710f2f5b03b
Andrew Smith <848smith@gmail.com>
2020-06-24 03:06:13 +0000

    Automatically backed up by Learn

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -1,5 +1,5 @@
 class PostValidator < ActiveModel::Validator
   def validate(record)
-    
+    record.errors[:post] << "We're only allowed to have people who work for the company in the database!"
   end
 end
