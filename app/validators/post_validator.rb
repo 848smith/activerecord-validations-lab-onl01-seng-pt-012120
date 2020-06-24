38713db7bed0c2655c58ea2880e81df5bdaaf4d0
Andrew Smith <848smith@gmail.com>
2020-06-24 03:12:16 +0000

    Automatically backed up by Learn

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -1,6 +1,6 @@
 class PostValidator < ActiveModel::Validator
   def validate(record)
-    if record.post.match?(/Won't Believe/)
+    if record.post.include?()
     record.errors[:post] << false
   end
 end
