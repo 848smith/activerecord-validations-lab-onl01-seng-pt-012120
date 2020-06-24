876e95d759699ccf11a0558735b716099d488f0c
Andrew Smith <848smith@gmail.com>
2020-06-24 03:09:25 +0000

    Automatically backed up by Learn

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -1,6 +1,6 @@
 class PostValidator < ActiveModel::Validator
   def validate(record)
     if record.post.match?(/Won't Believe/)
-    record.errors[:post] << "We're only allowed to have people who work for the company in the database!"
+    record.errors[:post] << false
   end
 end
