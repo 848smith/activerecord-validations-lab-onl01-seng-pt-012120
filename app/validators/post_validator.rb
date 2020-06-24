b86c2f772e0fd58d7e9f42ff9b15f00469635303
Andrew Smith <848smith@gmail.com>
2020-06-24 03:18:52 +0000

    Automatically backed up by Learn

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -1,6 +1,6 @@
 class PostValidator < ActiveModel::Validator
   def validate(record)
-    if record.post.include?("Won't Believe", "Secret", "Top [number]", "Guess")
+    if record.title.include?("Won't Believe", "Secret", "Top [number]", "Guess")
       record.errors[:post] << false
     end
   end
